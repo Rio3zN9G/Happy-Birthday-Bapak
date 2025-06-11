7862b45ae621fb7e753b473957cafb00e2bdc942
Rio <aryont52@gmail.com>
2025-06-12 06:28:04 +0700

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "Pak", "Agung", "🥳"],
+    strings: ["HAPPY", "BIRTHDAY!", "Pak", "Agung"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
